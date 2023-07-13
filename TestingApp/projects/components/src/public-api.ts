c59c6f53e914841c9a8ac1ee5efd0ad1169e5ddb
Philipp Parteder <88061157+PhilippFC@users.noreply.github.com>
2023-07-13 16:09:11 +0200

    added my-component, initialized up Angular-to-WC Converter Application

---
+++ TestingApp/projects/components/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of components
+ */
+
+export * from './lib/components.service';
+export * from './lib/my-component/my-component.component';
+export * from './lib/components.module';
