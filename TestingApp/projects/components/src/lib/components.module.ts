a3c886c9935b0dceb8ccfdccc87c3ac125c9f33c
Philipp Parteder <88061157+PhilippFC@users.noreply.github.com>
2023-08-14 11:38:43 +0200

    removed unnecessary files

--- TestingApp/projects/components/src/lib/components.module.ts
+++ TestingApp/projects/components/src/lib/components.module.ts
@@ -1,22 +1,11 @@
 import { NgModule } from '@angular/core';
-import { MyComponentComponent } from './my-component/my-component.component';
 import { TableComponent } from './table/table.component';
 import { MenuComponent } from './menu/menu.component';
 import { MenuItemComponent } from './menu-item/menu-item.component';
 
 @NgModule({
-  declarations: [
-    MyComponentComponent,
-    TableComponent,
-    MenuComponent,
-    MenuItemComponent,
-  ],
+  declarations: [TableComponent, MenuComponent, MenuItemComponent],
   imports: [],
-  exports: [
-    MyComponentComponent,
-    TableComponent,
-    MenuComponent,
-    MenuItemComponent,
-  ],
+  exports: [TableComponent, MenuComponent, MenuItemComponent],
 })
 export class ComponentsModule {}
